1e91d5ffc41fa98ddd23c445572141a6df70adba
Unknown <angelayu822@gmail.com>
2018-04-07 18:41:20 +0800

    家瑜-內文改用迴圈跑

---
+++ discuss-0404/routes/discuss2.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var pool = require('./lib/db.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+   pool.query('select * from book',function (error, results, fields) {
+        if (error){
+            res.render('discuss2', {data:[]});
+        }else{
+            res.render('discuss2', {data:results});
+        }       
+    });
+});
+
+module.exports = router;
---
+++ discuss-0404/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var pool = require('./lib/db.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+   pool.query('select * from book', function (error, results, fields) {
+        if (error){
+            res.render('discuss', {data:[]});
+        }else{
+            res.render('discuss', {data:results});
+        }       
+    });
+});
+
+module.exports = router;
